76dc77da03c887762f706de0f96e50f4e13a2b0d
Nicholas Ward <npward@berkeley.edu>
2022-05-06 17:15:16 -0700

    fix

--- system_zero/src/memory.rs
+++ system_zero/src/memory.rs
@@ -19,7 +19,6 @@ pub struct MemorySegment {
     pub content: Vec<u8>,
 }
 
-
 pub(crate) fn generate_memory<F: PrimeField64>(values: &mut [F; NUM_COLUMNS]) {
     todo!()
 }
@@ -41,11 +40,11 @@ pub(crate) fn eval_memory<F: Field, P: PackedField<Scalar = F>>(
     let next_from = vars.next_values[SORTED_MEMORY_FROM];
     let next_to = vars.next_values[SORTED_MEMORY_TO];
     let next_timestamp = vars.next_values[SORTED_MEMORY_TIMESTAMP];
-    
+
     let trace_context = vars.local_values[MEMORY_TRACE_CONTEXT];
     let trace_segment = vars.local_values[MEMORY_TRACE_SEGMENT];
     let trace_virtual = vars.local_values[MEMORY_TRACE_VIRTUAL];
-    
+
     let current = vars.local_values[MEMORY_CURRENT];
     let next_current = vars.next_values[MEMORY_CURRENT];
 
@@ -61,14 +60,29 @@ pub(crate) fn eval_memory<F: Field, P: PackedField<Scalar = F>>(
     let segment_range_check = vars.local_values[super::range_check_degree::col_rc_16_input(1)];
     let virtual_range_check = vars.local_values[super::range_check_degree::col_rc_16_input(2)];
 
-    yield_constr.constraint(context_range_check - trace_context * (next_addr_segment - addr_segment) - (F::ONE - trace_context) * (next_addr_context - addr_context - F::ONE));
-    yield_constr.constraint(segment_range_check - trace_segment * (next_addr_virtual - addr_virtual) - (F::ONE - trace_segment) * (next_addr_segment - addr_segment - F::ONE));
-    yield_constr.constraint(virtual_range_check - trace_virtual * (next_timestamp - timestamp) - (F::ONE - trace_virtual) * (next_addr_virtual - addr_virtual - F::ONE));
+    yield_constr.constraint(
+        context_range_check
+            - trace_context * (next_addr_segment - addr_segment)
+            - (F::ONE - trace_context) * (next_addr_context - addr_context - F::ONE),
+    );
+    yield_constr.constraint(
+        segment_range_check
+            - trace_segment * (next_addr_virtual - addr_virtual)
+            - (F::ONE - trace_segment) * (next_addr_segment - addr_segment - F::ONE),
+    );
+    yield_constr.constraint(
+        virtual_range_check
+            - trace_virtual * (next_timestamp - timestamp)
+            - (F::ONE - trace_virtual) * (next_addr_virtual - addr_virtual - F::ONE),
+    );
 
     // Enumerate purportedly-ordered log using current value c.
     yield_constr.constraint_first_row(current);
     yield_constr.constraint(current - from);
-    yield_constr.constraint(next_current - (F::ONE - trace_context) * (F::ONE - trace_segment) * (F::ONE - trace_virtual) * to);
+    yield_constr.constraint(
+        next_current
+            - (F::ONE - trace_context) * (F::ONE - trace_segment) * (F::ONE - trace_virtual) * to,
+    );
     // TODO: redo trace columns to fix this high-degree constraint ^
 }
 
@@ -76,5 +90,4 @@ pub(crate) fn eval_memory_recursively<F: RichField + Extendable<D>, const D: usi
     vars: StarkEvaluationTargets<D, NUM_COLUMNS, NUM_PUBLIC_INPUTS>,
     yield_constr: &mut ConstraintConsumer<P>,
 ) {
-    
 }
