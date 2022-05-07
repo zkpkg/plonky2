9156431a412579711a943afd66a02659f6cde0d7
Nicholas Ward <npward@berkeley.edu>
2022-05-06 17:14:19 -0700

    in-progress updates

--- system_zero/src/memory.rs
+++ system_zero/src/memory.rs
@@ -31,36 +31,45 @@ pub(crate) fn eval_memory<F: Field, P: PackedField<Scalar = F>>(
     let addr_context = vars.local_values[SORTED_MEMORY_ADDR_CONTEXT];
     let addr_segment = vars.local_values[SORTED_MEMORY_ADDR_SEGMENT];
     let addr_virtual = vars.local_values[SORTED_MEMORY_ADDR_VIRTUAL];
-    let val = vars.local_values[SORTED_MEMORY_VALUE];
+    let from = vars.local_values[SORTED_MEMORY_FROM]; // TODO: replace "from" and "to" with "val" and "R/W"
+    let to = vars.local_values[SORTED_MEMORY_TO];
     let timestamp = vars.local_values[SORTED_MEMORY_TIMESTAMP];
 
     let next_addr_context = vars.next_values[SORTED_MEMORY_ADDR_CONTEXT];
     let next_addr_segment = vars.next_values[SORTED_MEMORY_ADDR_SEGMENT];
     let next_addr_virtual = vars.next_values[SORTED_MEMORY_ADDR_VIRTUAL];
-    let next_val = vars.next_values[SORTED_MEMORY_VALUE];
+    let next_from = vars.next_values[SORTED_MEMORY_FROM];
+    let next_to = vars.next_values[SORTED_MEMORY_TO];
     let next_timestamp = vars.next_values[SORTED_MEMORY_TIMESTAMP];
     
-    let memory_trace_context = vars.local_values[MEMORY_TRACE_CONTEXT];
-    let memory_trace_segment = vars.local_values[MEMORY_TRACE_SEGMENT];
-    let memory_trace_virtual = vars.local_values[MEMORY_TRACE_VIRTUAL];
-    let memory_current = vars.local_values[MEMORY_CURRENT];
+    let trace_context = vars.local_values[MEMORY_TRACE_CONTEXT];
+    let trace_segment = vars.local_values[MEMORY_TRACE_SEGMENT];
+    let trace_virtual = vars.local_values[MEMORY_TRACE_VIRTUAL];
+    
+    let current = vars.local_values[MEMORY_CURRENT];
+    let next_current = vars.next_values[MEMORY_CURRENT];
 
-    yield_constr.constraint(memory_trace_context * (F::ONE - memory_trace_context));
-    yield_constr.constraint(memory_trace_segment * (F::ONE - memory_trace_segment));
-    yield_constr.constraint(memory_trace_virtual * (F::ONE - memory_trace_virtual));
+    yield_constr.constraint(trace_context * (F::ONE - trace_context));
+    yield_constr.constraint(trace_segment * (F::ONE - trace_segment));
+    yield_constr.constraint(trace_virtual * (F::ONE - trace_virtual));
 
-    yield_constr.constraint(memory_trace_context * (next_addr_context - addr_context));
-    yield_constr.constraint(memory_trace_segment * (next_addr_segment - addr_segment));
-    yield_constr.constraint(memory_trace_virtual * (next_addr_virtual - addr_virtual));
+    yield_constr.constraint(trace_context * (next_addr_context - addr_context));
+    yield_constr.constraint(trace_segment * (next_addr_segment - addr_segment));
+    yield_constr.constraint(trace_virtual * (next_addr_virtual - addr_virtual));
 
     let context_range_check = vars.local_values[super::range_check_degree::col_rc_16_input(0)];
     let segment_range_check = vars.local_values[super::range_check_degree::col_rc_16_input(1)];
     let virtual_range_check = vars.local_values[super::range_check_degree::col_rc_16_input(2)];
 
-    yield_constr.constraint(context_range_check - memory_trace_context * (next_addr_segment - addr_segment) - (F::ONE - memory_trace_context) * (next_addr_context - addr_context - F::ONE));
-    yield_constr.constraint(segment_range_check - memory_trace_segment * (next_addr_virtual - addr_virtual) - (F::ONE - memory_trace_segment) * (next_addr_segment - addr_segment - F::ONE));
-    yield_constr.constraint(virtual_range_check - memory_trace_virtual * (next_timestamp - timestamp) - (F::ONE - memory_trace_virtual) * (next_addr_virtual - addr_virtual - F::ONE));
+    yield_constr.constraint(context_range_check - trace_context * (next_addr_segment - addr_segment) - (F::ONE - trace_context) * (next_addr_context - addr_context - F::ONE));
+    yield_constr.constraint(segment_range_check - trace_segment * (next_addr_virtual - addr_virtual) - (F::ONE - trace_segment) * (next_addr_segment - addr_segment - F::ONE));
+    yield_constr.constraint(virtual_range_check - trace_virtual * (next_timestamp - timestamp) - (F::ONE - trace_virtual) * (next_addr_virtual - addr_virtual - F::ONE));
 
+    // Enumerate purportedly-ordered log using current value c.
+    yield_constr.constraint_first_row(current);
+    yield_constr.constraint(current - from);
+    yield_constr.constraint(next_current - (F::ONE - trace_context) * (F::ONE - trace_segment) * (F::ONE - trace_virtual) * to);
+    // TODO: redo trace columns to fix this high-degree constraint ^
 }
 
 pub(crate) fn eval_memory_recursively<F: RichField + Extendable<D>, const D: usize>(
--- system_zero/src/registers/memory.rs
+++ system_zero/src/registers/memory.rs
@@ -3,16 +3,16 @@
 pub(crate) const MEMORY_ADDR_CONTEXT: usize = super::START_MEMORY;
 pub(crate) const MEMORY_ADDR_SEGMENT: usize = MEMORY_ADDR_CONTEXT + 1;
 pub(crate) const MEMORY_ADDR_VIRTUAL: usize = MEMORY_ADDR_SEGMENT + 1;
-pub(crate) const MEMORY_VALUE: usize = MEMORY_ADDR_VIRTUAL + 1;
-pub(crate) const MEMORY_READ_OR_WRITE: usize = MEMORY_VALUE + 1;
-pub(crate) const MEMORY_TIMESTAMP: usize = MEMORY_READ_OR_WRITE + 1;
+pub(crate) const MEMORY_FROM: usize = MEMORY_ADDR_VIRTUAL + 1;
+pub(crate) const MEMORY_TO: usize = MEMORY_FROM + 1;
+pub(crate) const MEMORY_TIMESTAMP: usize = MEMORY_TO + 1;
 
 pub(crate) const SORTED_MEMORY_ADDR_CONTEXT: usize = MEMORY_TIMESTAMP + 1;
 pub(crate) const SORTED_MEMORY_ADDR_SEGMENT: usize = SORTED_MEMORY_ADDR_CONTEXT + 1;
 pub(crate) const SORTED_MEMORY_ADDR_VIRTUAL: usize = SORTED_MEMORY_ADDR_SEGMENT + 1;
-pub(crate) const SORTED_MEMORY_VALUE: usize = SORTED_MEMORY_ADDR_VIRTUAL + 1;
-pub(crate) const SORTED_MEMORY_READ_OR_WRITE: usize = SORTED_MEMORY_VALUE + 1;
-pub(crate) const SORTED_MEMORY_TIMESTAMP: usize = SORTED_MEMORY_READ_OR_WRITE + 1;
+pub(crate) const SORTED_MEMORY_FROM: usize = SORTED_MEMORY_ADDR_VIRTUAL + 1;
+pub(crate) const SORTED_MEMORY_TO: usize = SORTED_MEMORY_FROM + 1;
+pub(crate) const SORTED_MEMORY_TIMESTAMP: usize = SORTED_MEMORY_TO + 1;
 
 pub(crate) const MEMORY_TRACE_CONTEXT: usize = SORTED_MEMORY_TIMESTAMP + 1;
 pub(crate) const MEMORY_TRACE_SEGMENT: usize = MEMORY_TRACE_CONTEXT + 1;
